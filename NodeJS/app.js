8403bb83bc67508a23708d5fe2b82330d550a268
joel-2004 <jrjoel484@gmail.com>
2023-09-05 07:06:48 +0530

    commit

---
+++ NodeJS/app.js
@@ -0,0 +1,13 @@
+// const { writeFileSync } = require("fs");
+// for (let i = 0; i <= 1000; i++) {
+//     writeFileSync("./content/BigFile.txt", `Hello ${i}`, { flag: 'a' });
+// } creating a big file to use Streams
+//stream uses eventEmitter
+//in this example we are using stream to read large datas from a file, we will not be able to do it with just readFile
+const { createReadStream } = require("fs");
+
+const stream = createReadStream("./content/BigFile.txt");
+
+stream.on("data", (result) => {
+    console.log(result);
+})
