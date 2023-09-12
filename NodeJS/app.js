c1ba1df550c2ac57778b3c5f44b87811efa17563
joel-2004 <jrjoel484@gmail.com>
2023-09-12 07:30:42 +0530

    Express begin

---
+++ NodeJS/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+//app.get read data from user
+//app.post insert data
+//app.put update data
+//app.delete delete data
+app.listen(5000, () => {
+    console.log("Listening at port 5000");
+})
