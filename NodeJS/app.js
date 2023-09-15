a6141fa61d19a78e8488fcfcc102c644a35e4f7b
joel-2004 <jrjoel484@gmail.com>
2023-09-15 07:06:06 +0530

    Api basics

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
-
+const { products } = require("../NodeJS/content/product")
 app.get("/", (req, res) => {
+    // res.json(products)
+    res.send(`<h1>Home Page</h1>  <a href="/api/products">Link</a>`)
+})
 
+app.get("/api/products", (req, res) => {
+    const newProduct = products.map((p) => {
+        const { id, name, image } = p;
+        return { id, name, image };
+    })
+    res.send(newProduct);
+})
+
+app.get("/api/products/1", (req, res) => {
+    const newProduct = products.find((p) => p.id == 1);
+
+    res.send(newProduct);
 })
 
 app.listen(5000, () => {
